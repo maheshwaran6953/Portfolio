b83ea83a0919b6ac02a0ad3969cd76179da0b7d6
maheshwaran6953 <maheshwaranpalanisamy1@gmail.com>
2026-02-12 19:44:14 +0530

    updated map in contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,6 +39,11 @@ export function Contact(){
     return(
         <div className="contact-page">
             <div className="contact-form">
+                <section class="mapbox" data-mapbox>
+                    <figure>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d489.52690798476544!2d77.5521156!3d11.0224696!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1770904598857!5m2!1sen!2sin" loading="lazy" ></iframe>
+                    </figure>
+                </section>
                 <h3 className='form-title'>Contact Form</h3>
                 <form action="#" className='form' onSubmit={handleSubmit}>
                     <div className="input-wrapper">
