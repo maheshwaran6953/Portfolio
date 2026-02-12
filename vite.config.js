239f9b8e0c59da1ea837f70687e5f2d94fa2fbb2
maheshwaran6953 <maheshwaranpalanisamy1@gmail.com>
2026-02-12 21:41:19 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/'  // CRITICAL: This tells Vite your site is in /portfolio/
+  base: '/'  // CRITICAL: This tells Vite your site is in /portfolio/
 })
